c4ff1ef6f908fe30ac818115e6f4d7a0cfcf0d2f
CezaryS0 <cezary.szumerowski@gmail.com>
2022-11-11 15:48:22 +0000

    Updates

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-from CNN.CNN_V1 import Encoder
+#from CNN.CNN_V1 import Encoder
+from CNN.CNN_V2 import Encoder
 
 enc = Encoder()
 enc.train_model('/content/drive/MyDrive/Train_Data','slices/tempo_bpm.npy')
