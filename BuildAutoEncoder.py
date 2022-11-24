85b013878e347f8bc54d0c3148c3f3e07c707247
CezaryS0 <cezary.szumerowski@gmail.com>
2022-11-24 13:26:07 +0000

    Updates

--- BuildAutoEncoder.py
+++ BuildAutoEncoder.py
@@ -1,4 +1,4 @@
 from Autoencoder.Autoencoder import Autoencoder
 
 autoenoder = Autoencoder()
-autoenoder.build_autoencoder('Train_Data/Train','Autoencoder_Saved')
+autoenoder.build_autoencoder('/content/drive/MyDrive/test/Train_Data/Train','Autoencoder_Saved')
