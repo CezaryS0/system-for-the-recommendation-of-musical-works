e445f0edb423af99277ce0cc0b816afe9abf6561
Cezary Szumerowski <czarek.szumerowski@gmail.com>
2023-01-16 19:45:05 +0100

    Final

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -11,7 +11,7 @@ class Controller:
         self.numpy = NumpyArray()
         self.encoder2D = ModelManager()
         self.encoder1D = ModelManager()
-        self.googleDrive = GDManager()
+        #self.googleDrive = GDManager()
         self.SQL_DB = Database()
         self.audio = Audio()
         self.timer = Timer()
@@ -23,16 +23,16 @@ class Controller:
         self.encoder2D.discard_layers(-14)
         data = self.numpy.read_sliced_spectrograms('Spectrograms')
         self.numpy.save_spectrogram_representations(data,self.encoder2D,self.output_folder)
-        self.googleDrive.upload_file_to_folder('Test',self.output_folder+'/Test/representations.npy',self.input_id)
-        self.googleDrive.upload_file_to_folder('Train',self.output_folder+'/Train/representations.npy',self.input_id)
+        #self.googleDrive.upload_file_to_folder('Test',self.output_folder+'/Test/representations.npy',self.input_id)
+        #self.googleDrive.upload_file_to_folder('Train',self.output_folder+'/Train/representations.npy',self.input_id)
 
     def generate_data_fusion_and_upload(self):
         data = self.numpy.read_sliced_spectrograms('Spectrograms')
         data.representations_test = self.numpy.read_numpy_file(self.output_folder+'/Test','representations.npy')
         data.representations_train = self.numpy.read_numpy_file(self.output_folder+'/Train','representations.npy')
         self.numpy.save_data_fusion(data,self.output_folder)
-        self.googleDrive.upload_file_to_folder('Test',self.output_folder+'/Test/fusion.npy',self.input_id)
-        self.googleDrive.upload_file_to_folder('Train',self.output_folder+'/Train/fusion.npy',self.input_id)
+        #self.googleDrive.upload_file_to_folder('Test',self.output_folder+'/Test/fusion.npy',self.input_id)
+        #self.googleDrive.upload_file_to_folder('Train',self.output_folder+'/Train/fusion.npy',self.input_id)
 
     def save_sliced_spectrograms_and_upload(self):
         self.numpy.save_dataset_to_numpy_files("Spectrograms",self.output_folder)
